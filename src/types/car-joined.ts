4991aba78502c4feb9c5d55a8d0f0b440c6be372
Greta <gretaapul@gmail..com>
2022-07-21 10:33:18 +0300

    helper types, methods

--- src/types/car-joined.ts
+++ src/types/car-joined.ts
@@ -1,9 +1,13 @@
+import Brand from './brand';
+import Car from './car';
+import Model from './model';
+
 type CarJoined = {
-  id: string;
-  price: number;
-  year: number;
-  brand: string;
-  model: string;
+  id: Car['id'];
+  price: Car['price'];
+  year: Car['year'];
+  brand: Brand['title'];
+  model: Model['title'];
 };
 
 export default CarJoined;
